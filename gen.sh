786b9711baac0e774e056bb62f17673d84866300
David Holdeman <david@chuckwagoncomputing.com>
2023-02-04 12:35:00 -0600

    try escaping

--- gen.sh
+++ gen.sh
@@ -12,6 +12,7 @@ for C in $COLORS; do
   fi
 done
 CSS+=$(cat $DIR/style.css)
+CSS=$(echo "$CSS" | sed 's/@/\\@/g')
 
 TEXT=$(sed -e "/###JS###/{r ${DIR}script.js" -e 'd}' ${DIR}pinout.html | sed -e "s/\/\/\/DATA\/\/\//\`$(echo ${JSON//\//\\/} | tr -d '\n')\`,\n\/\/\/DATA\/\/\//" | perl -0pe "s/###CSS###/${CSS}/" | perl -0pe "s/\/\/\/COLS\/\/\//${COLS}/" | perl -0pe "s/\/\/\/PRINT_COLS\/\/\//${PRINT_COLS}/")
 if [ $? -ne 0 ]; then
