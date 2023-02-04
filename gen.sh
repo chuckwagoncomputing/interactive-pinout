cca998ed1bdd3a22f2c2e1d1e7406c7f1fed4550
David Holdeman <david@chuckwagoncomputing.com>
2023-02-04 11:32:01 -0600

    oops close

--- gen.sh
+++ gen.sh
@@ -13,7 +13,7 @@ for C in $COLORS; do
 done
 CSS+=$(cat $DIR/style.css)
 
-TEXT=$(sed -e "/###JS###/{r ${DIR}script.js" -e 'd}' ${DIR}pinout.html | sed -e "s/\/\/\/DATA\/\/\//\`$(echo ${JSON//\//\\/} | tr -d '\n')\`,\n\/\/\/DATA\/\/\//" | perl -0pe "s/###CSS###/${CSS}/" | perl -0pe "s/\/\/\/COLS\/\/\//${COLS}/" | perl -0pe "s/\/\/\/PRINT_COLS\/\/\//${PRINT_COLS}/"
+TEXT=$(sed -e "/###JS###/{r ${DIR}script.js" -e 'd}' ${DIR}pinout.html | sed -e "s/\/\/\/DATA\/\/\//\`$(echo ${JSON//\//\\/} | tr -d '\n')\`,\n\/\/\/DATA\/\/\//" | perl -0pe "s/###CSS###/${CSS}/" | perl -0pe "s/\/\/\/COLS\/\/\//${COLS}/" | perl -0pe "s/\/\/\/PRINT_COLS\/\/\//${PRINT_COLS}/")
 if [ $? -ne 0 ]; then
   echo "Error in gen.sh"
   exit 1;
