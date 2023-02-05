e322a6f0def5a9030a913d1c6e12e0a74fc0f2b7
David Holdeman <david@chuckwagoncomputing.com>
2023-02-04 20:44:06 -0600

    comments

--- script.js
+++ script.js
@@ -183,7 +183,9 @@ window.addEventListener('load', function() {
       checkparams();
     }
   };
+  // For every connector...
   for (var c = 0; c < connectorData.length; c++) {
+    // Parse the JSON, add connector to document body
     connectorData[c] = JSON.parse(connectorData[c]);
     var connector = connectorData[c];
     var template = document.getElementById("connector-template");
@@ -191,7 +193,9 @@ window.addEventListener('load', function() {
     document.body.appendChild(clone);
     var sdiv = document.body.lastElementChild;
     var img = sdiv.querySelector(".connector-img");
+    // Increment "images we need to load" counter
     images += 1;
+    // When the image is loaded, then handle the pins
     img.addEventListener('load', function(connector, sdiv, img) {
       var cdiv = sdiv.querySelector(".connector-div");
       var cid = connector.info.id;
@@ -202,17 +206,20 @@ window.addEventListener('load', function() {
       var infoTableHeader = sdiv.querySelector(".info-table").querySelector("thead>tr");
       var fullTable = sdiv.querySelector(".pinout-table").querySelector("tbody");
       var fullTableHeader = sdiv.querySelector(".pinout-table").querySelector("thead>tr");
+      // Loop through our columns and add the headers for both tables
       for (const column in columns) {
         var el = document.createElement("th");
         el.textContent = columns[column];
         infoTableHeader.appendChild(el.cloneNode(true));
         fullTableHeader.appendChild(el.cloneNode(true));
       }
+      // For every pin...
       for (var i = 0; i < connector.pins.length; i++) {
         var pin = connector.pins[i];
         if (!pin.pin) {
           continue;
         }
+        // Get the pin info from the info section (i.e. x/y coordinates)
         var pinfo = {};
         for (var ii = 0; ii < connector.info.pins.length; ii++) {
           if (connector.info.pins[ii].pin == pin.pin) {
@@ -220,10 +227,13 @@ window.addEventListener('load', function() {
             break;
           }
         }
+        // If there aren't coordinates, just add to the table
         if (!pinfo.x) {
           addRow(fullTable, connector.pins[i], cid);
           continue;
         }
+        // Find the closest pin, to maximize the pin size for best readability,
+        //    without overlapping pins.
         var closest = 1000000;
         for (var ii = 0; ii < connector.info.pins.length; ii++) {
           var tinfo = connector.info.pins[ii];
@@ -232,16 +242,19 @@ window.addEventListener('load', function() {
             closest = distance;
           }
         }
+        // Create the pin element and set its position and type
         var pclone = ptemplate.content.cloneNode(true);
         var pdiv = pclone.querySelector("div");
         pdiv.textContent = pinfo.pin;
         pdiv.style.top = ((pinfo.y / imgHeight) * 100) + "%";
         pdiv.style.left = ((pinfo.x / imgWidth) * 100) + "%";
         pdiv.dataset.type = pin.type;
+        // Associate the pin's element with the pin object
         pin.pdiv = pdiv;
         pdiv.addEventListener("click", function(table, pin, cid) {
           clickPin(table, pin, cid);
         }.bind(null, infoTable, pin, cid));
+        // Set the pin's size
         closest = Math.sqrt(closest);
         var divheight = cdiv.clientHeight;
         var divwidth = cdiv.clientWidth;
@@ -259,6 +272,7 @@ window.addEventListener('load', function() {
         pdiv.style.marginLeft = "-" + (width / 2) + "%";
         pdiv.style.fontSize = (height * 1.8) + "px";
         pdiv.style.fontSize = (pxheight * 0.5) + "px";
+        // Recalculate the size for printing.
         window.addEventListener('beforeprint', function(pdiv, width, divwidth, event) {
           pdiv.style.fontSize = "calc(calc(" + width + "px * min(640, "  + divwidth + ")) * 0.0055)";
         }.bind(null, pdiv, width, divwidth));
@@ -269,20 +283,27 @@ window.addEventListener('load', function() {
         addRow(fullTable, pin, cid);
       }
       hideEmptyColumns(sdiv.querySelector('.pinout-table'));
+      // Check if we have loaded all the images.
       checkImagesLoaded();
     }.bind(null, connector, sdiv, img));
+    // If there's info, use it.
     if (typeof(connector.info) != "undefined") {
+      // Add the image to load it
       img.src = connector.info.image.file;
+      // Set the document title
       if (document.title.length == 0 && typeof(connector.info.title) != "undefined") {
         document.title = connector.info.title;
       }
+      // Add the board link
       if (typeof(connector.info.board_url) != "undefined" && document.title.length > 0) {
         document.getElementById("board-link").innerText = document.title;
         document.getElementById("board-link").href = connector.info.board_url;
       }
+      // Add the connector name
       if (typeof(connector.info.name) != "undefined") {
         sdiv.querySelector(".connector-name").innerText = connector.info.name;
       }
+    // If there's no info, just build the table.
     } else {
       img.parentElement.parentElement.style.height = 0;
       for (var i = 0; i < connector.pins.length; i++) {
