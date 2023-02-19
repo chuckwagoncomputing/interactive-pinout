00e22c472874192f92b111e155ba51e0fe19a1ce
David Holdeman <david@chuckwagoncomputing.com>
2023-02-18 23:28:54 -0600

    Oh for Pete's Sake

--- main.sh
+++ main.sh
@@ -86,11 +86,11 @@ for c in $CONNECTORS; do
   echo "File Name: $NAME"
   if [ "$(yq e '.info.cid' "$c")" == "null" ]; then
     echo "WARNING: Missing yaml cid field in info section of $c"
-    if [ "$WARNINGS" = "error" ] && [ "WARNING_NO_CID" = "unset" ] || [ "$WARNING_NO_CID" = "error" ]; then
+    if [ "$WARNINGS" = "error" ] && [ "$WARNING_NO_CID" = "unset" ] || [ "$WARNING_NO_CID" = "error" ]; then
       exit 1;
-    elif [ "$WARNINGS" = "notice" ] && [ "WARNING_NO_CID" = "unset" ] || [ "$WARNING_NO_CID" = "notice" ]; then
+    elif [ "$WARNINGS" = "notice" ] && [ "$WARNING_NO_CID" = "unset" ] || [ "$WARNING_NO_CID" = "notice" ]; then
       echo "::notice:: Missing yaml cid field in info section of $c"
-    elif [ "$WARNINGS" = "skip" ] && [ "WARNING_NO_CID" = "unset" ] || [ "$WARNING_NO_CID" = "skip" ]; then
+    elif [ "$WARNINGS" = "skip" ] && [ "$WARNING_NO_CID" = "unset" ] || [ "$WARNING_NO_CID" = "skip" ]; then
       continue
     fi
   fi
@@ -112,11 +112,11 @@ for c in $CONNECTORS; do
   IMG=$(yq e '.info.image.file' "$c")
   if [ $? -ne 0 ] || [ "$IMG" = "null" ]; then
     echo "WARNING: Missing image"
-    if [ "$WARNINGS" = "error" ] && [ "WARNING_NO_IMAGE" = "unset" ] || [ "$WARNING_NO_IMAGE" = "error" ]; then
+    if [ "$WARNINGS" = "error" ] && [ "$WARNING_NO_IMAGE" = "unset" ] || [ "$WARNING_NO_IMAGE" = "error" ]; then
       exit 1;
-    elif [ "$WARNINGS" = "notice" ] && [ "WARNING_NO_IMAGE" = "unset" ] || [ "$WARNING_NO_IMAGE" = "notice" ]; then
+    elif [ "$WARNINGS" = "notice" ] && [ "$WARNING_NO_IMAGE" = "unset" ] || [ "$WARNING_NO_IMAGE" = "notice" ]; then
       echo "::notice:: $c Missing image"
-    elif [ "$WARNINGS" = "skip" ] && [ "WARNING_NO_IMAGE" = "unset" ] || [ "$WARNING_NO_IMAGE" = "skip" ]; then
+    elif [ "$WARNINGS" = "skip" ] && [ "$WARNING_NO_IMAGE" = "unset" ] || [ "$WARNING_NO_IMAGE" = "skip" ]; then
       continue
     fi
   else
